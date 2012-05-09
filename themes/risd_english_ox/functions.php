d16486662924e6adfb8386e65f0e8d038247fe03
danielwiener <daniel@danielwiener.com>
2012-05-09 11:48:14 -0400

    trying Oxygen based on Hybrid Core as the parent theme

---
+++ themes/risd_english_ox/functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package RISD English (Hybrid and Oxygen)
+ * @subpackage Functions
+ * @version 0.2.2
+ * @author Daniel Wiener
+ * @link http://devpress.com
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+add_theme_support( 'theme-layouts', array( '2c-l', '2c-r' ) );   //deal with this later, if necessary OR DELETE!
