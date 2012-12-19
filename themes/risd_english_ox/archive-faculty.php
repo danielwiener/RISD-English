cc2aa0b3180d5f31e6bff174f7aeedc5e40f00db
Daniel Wiener <daniel@danielwiener.com>
2012-12-19 15:39:56 -0500

    deleting "atomic" code that is unnecessary. Adding code for Senior Lecturers and Full Time Faculty

--- themes/risd_english_ox/archive-faculty.php
+++ themes/risd_english_ox/archive-faculty.php
@@ -18,9 +18,7 @@ get_header(); // Loads the header.php template. ?>
 	
 	</div>
 	
-	<div class="content-wrap">
-		
-		<?php do_atomic( 'before_content' ); // oxygen_before_content ?>
+	<div class="content-wrap">	
 
 		<div id="content">
 	
@@ -46,7 +44,7 @@ get_header(); // Loads the header.php template. ?>
 					); 
 				 	query_posts( $args ); ?>
 				<?php if ( have_posts() ) : ?>
-	
+						<h2>Full Time Faculty</h2>
 					<?php while ( have_posts() ) : the_post(); ?>
 	
 						<?php do_atomic( 'before_entry' ); // oxygen_before_entry ?>
@@ -82,13 +80,9 @@ get_header(); // Loads the header.php template. ?>
 								
 							</div>
 								
-							<a class="read-more" href="<?php the_permalink(); ?>">Read About <?php echo $dw_first_name . ' ' . get_the_title() ?> &rarr;</a>
+							<a class="read-more" href="<?php the_permalink(); ?>">Read About <?php echo $dw_first_name . ' ' . get_the_title() ?> &rarr;</a>						
 	
-							<?php do_atomic( 'close_entry' ); // oxygen_close_entry ?>
-	
-						</div><!-- .hentry -->
-	
-						<?php do_atomic( 'after_entry' ); // oxygen_after_entry ?>
+						</div><!-- .hentry -->	
 	
 					<?php endwhile; ?>
 	
@@ -99,13 +93,82 @@ get_header(); // Loads the header.php template. ?>
 				<?php endif; ?>
 	
 			</div><!-- .hfeed -->
+			<div class="hfeed">
+	             <?php
+					global $wp_query;
+					$args = array_merge( $wp_query->query,
+						array(
+							'orderby'         	=> 'title',
+							'order'           	=> 'ASC',
+							'posts_per_page'	=> -1,
+							'tax_query' => array(
+									array(
+										'taxonomy' => 'faculty_category',
+										'field' => 'slug',
+										'terms' => 'senior-lecturer'
+									)
+								),
+						   
+						)	
+					); 
+				 	query_posts( $args ); ?>
+				<?php if ( have_posts() ) : ?>
+					
+					<br clear="both" />
+					<hr />
+						<h2>Senior Lecturers</h2>
+					<?php while ( have_posts() ) : the_post(); ?>
 	
-			<?php // do_atomic( 'close_content' ); // oxygen_close_content ?>
+						<?php do_atomic( 'before_entry' ); // oxygen_before_entry ?>
 	
-			<?php // get_template_part( 'loop-nav' ); // Loads the loop-nav.php template. ?>
+						<div id="post-<?php the_ID(); ?>" class="<?php hybrid_entry_class(); ?>">		
+	
+							<?php do_atomic( 'open_entry' ); // oxygen_open_entry ?>
+	
+							
+							<div class="entry-header">
+								<?php if ( get_post_meta($post->ID, "_dw_first_name", true) ): ?>
+									<?php $dw_first_name = get_post_meta($post->ID, "_dw_first_name", true); ?>
+									<h2 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php echo $dw_first_name . '  ' . the_title(); ?>" rel="bookmark"><?php echo $dw_first_name; ?> <?php the_title(); ?></a></h2>
+								<?php else: ?>
+									<div class="alert alert-block">
+								    <!-- <a class="close" data-dismiss="alert" href="#">×</a> --><!-- Need to add bootstrap js and more classes to get this to work                                                                                  -->
+								    <h4 class="alert-heading">Warning!</h4>
+								     Add  first name to the first name field.
+								    </div>
+								   
+								<?php endif ?> 
+							</div>
+								
+							<div class="entry-summary">
+									<?php if ( current_theme_supports( 'get-the-image' ) ) {
+
+										get_the_image( array( 'meta_key' => 'Thumbnail', 'size' => 'portrait-thumbnail', 'image_class' => 'alignleft', 'width' => 75, 'height' => 90, 'attachment' => false, 'default_image' => get_stylesheet_directory_uri() . '/images/default_portrait_placeholder.gif' ) );							
+
+									} ?>  
+								<?php the_excerpt(); ?>
+								
+								<?php wp_link_pages( array( 'before' => '<p class="page-links">' . __( 'Pages:', hybrid_get_parent_textdomain() ), 'after' => '</p>' ) ); ?>
+								
+							</div>
+								
+							<a class="read-more" href="<?php the_permalink(); ?>">Read About <?php echo $dw_first_name . ' ' . get_the_title() ?> &rarr;</a>						
+	
+						</div><!-- .hentry -->	
+	
+					<?php endwhile; ?>
+	
+				<?php else : ?>
+	
+					<?php get_template_part( 'loop-error' ); // Loads the loop-error.php template. ?>
+	
+				<?php endif; ?>
+	
+			</div><!-- .hfeed -->
+
 	
 		</div><!-- #content -->
 	
-		<?php // do_atomic( 'after_content' ); // oxygen_after_content ?>
+
 
 <?php get_footer(); // Loads the footer.php template. ?>
