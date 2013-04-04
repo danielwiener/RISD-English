13a58fab45eb7aaac0f6403d41d44b0cce986f05
Daniel Wiener <daniel@danielwiener.com>
2013-04-04 16:34:13 -0400

    modified LAS pages help

---
+++ themes/risd_english_ox/lib/documentation/page_hierarchy.php
@@ -0,0 +1,46 @@
+<h2>Contact</h2>
+<p>Change text, as needed. Copy and paste to create a new address to keep formatting.
+</p>
+<hr />
+<h2>E101 Prize</h2>
+<p>E101 Prize is a parent page. Edit this page by hand and change text, as needed. It contains a list of winners with links to a page with the winner's portrait and prizing winning essay. Add new winners to the list and link them to their webpage. (You should probably create a page from them first, see below)
+</p>
+<p>
+<strong>E101 Prize Winners</strong><br>
+To create a new prize winner, make a new page and choose "E101 Prize" from "Page Attributes" subpanel at the right. 
+Add a portrait image, use a small-ish image, not to exceed 300 pixels. WP will create a medium sized image which is cropped so as not to exceed 300 pixels in either direction. 
+Make the image left-aligned. Add text and a link to an uploaded pdf of their paper. Check the formatting of past winners and copy it.
+</p>
+<hr />
+<h2>Home</h2>
+<p>Don't edit or delete. This page is needed to create the home page.</p>
+<hr />
+<h2>Mission and Curriculum</h2>
+<p><strong>Mission and Curriculum</strong> is a parent page beginning with text explaining the mission of LAS. It continues with a listing of the primary areas of study in the LAS curriculum. It will be edited infrequently. The curriculum is formatted with a headline for an area of study, an inserted image representing it, followed by a paragraph describing the area of study and ending with another paragraph describing a highlighted class. Check the formatting of existing page to keep it consistent.</p>
+
+<h3>Children of Mission and Curriculum</h3>
+<p><strong>Concentration </strong>- description of Concentrating in LAS. Edit text as needed.</p>
+
+<p><strong>Current Courses</strong> - this page reads the Course Feed produced by OIT. The course list is automatically created from the feed except for LAS 101.  Edit text for LAS 101 courses for Fall and Spring in the 2 text boxes below the main text box. These will not need to be edited often. <br></p>
+(Tweaks to the programming in order to parse the OIT feed may be needed, contact Daniel if this page does not work.)
+<p><strong>Required Courses</strong> - Edit as needed.</p>
+<hr />
+<h2>Newsletter</h2>
+<p>
+<strong>Newsletter</strong> - change and add text, usually just a line which links to an uploaded pdf. See http://documentation.diyartportfolios.com/posts/uploading-a-pdf for uploading pdfs.
+</p>
+<hr />
+<h2>Students</h2>
+<p>
+<strong>Students</strong> - don't edit or delete, this page is used to create the students TOC page - http://risd-english.com/students
+</p>
+<h3>Children of "Students"</h3>
+<p>
+<strong>Current Concentrators</strong> - these pages will change each year. One or 2 students are highlighted with a medium sized portrait image, and text and links. Copy the formatting of this page for future changes.
+</p>
+<p>
+<strong>Student Work</strong> - this page will change yearly (probably). It uses the template called "Two Columns" in the Page Attributes subpanel. Add images, text and sound as needed. Add SoundCloud --- by using the Add Media button and clicking on SoundCloud. The content and look of this page will change from year to year. Use all the basic capabilities of Wordpress.</p>
+
+<p>
+<strong>Alumni Concentrators</strong> - This is essentially the same as the Current Concentrators page and includes a portrait image, text and links for alumni. It may change less frequently than the Current Concentrators page. 
+</p>
