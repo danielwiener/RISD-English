3e70db16895443e3d578021a2e82203fe108f878
Daniel Wiener <daniel@danielwiener.com>
2013-04-04 16:33:08 -0400

    modified elective help page

---
+++ themes/risd_english_ox/lib/documentation/elective_help.php
@@ -0,0 +1,17 @@
+<h2>Electives: How to Add a New Elective Genre Page</h2>
+
+<p>On the left toolbar, select <strong>Electives, Add New.</strong></p>
+
+Enter the <strong>Elective Genre Title</strong>. (For example, "British Literature" or "World Literature.")
+<p>Enter the <strong>Excerpt</strong>. This is the descriptive overview of the Elective genre that will appear alongside the Featured Image on both the Table of Contents page, and the Individual Elective page.</p>
+
+<p>**<strong>Important Note:</strong> Elective pages differ from other sections in that the Excerpt text will appear <em>both</em> on the <a href="http://risd-english.com/electives" target="_blank">Table of Contents</a> page beneath the Featured Image, as well as on <a href="risd-english.com/electives/courses-in-british-and-american-literature" target="_blank">Individual Elective</a> page, to the right of the Featured Image. **</p>
+
+<p>Enter the <strong>content</strong> (class list) in the Main Text Box. Here, the main content is the specific list of courses and course numbers. (For example, "E208 Canterbury Tales" or "E201 The Bible as Narrative Art.") The main content will appear beneath the Excerpt, below the horizontal bar.</p>
+
+
+<p>Enter the <strong>Featured Image</strong>. For the Table of Contents page, this image will be cropped into a thumbnail, 470 x 140 pixels. Choose an image with action in the middle, because WordPress will automatically crop to the center chopping off the edges of the longest dimension.</p>
+
+<p><strong>Note the Sidebar</strong> - this is the list of the Elective Genres that will stay visible on the right side of the screen.</p>
+
+
