55373e7bacdef7a997176e02585852f8de6c0011
Daniel Wiener <daniel@danielwiener.com>
2013-04-04 16:30:56 -0400

    added general instructions and links and faculty instructions here

--- mu-plugins/dashboard_private_pages.php
+++ mu-plugins/dashboard_private_pages.php
@@ -34,9 +34,24 @@ Author URI: http://danielwiener.com/
 // from http://erisds.co.uk/wordpress/snippet-wordpress-admin-tidy-dashboard-widgets-by-role
 
 function display_dashboard_widget()
-{ ?>
-	<h4>Instructions Coming Soon</h4>
-<?php
+{ 
+	$widget_content =  '<h4>QuickStart Instructions</h4>';
+	$widget_content .= '<ol>
+	<li>Once signed into the Dashboard, navigate to <a href="/wp-admin/edit.php?post_type=exhibitions">Exhibitions</a>, <a href="/wp-admin/edit.php?post_type=faculty">Faculty</a>, <a href="/wp-admin/edit.php?post_type=electives">Electives</a>, <a href="/wp-admin/edit.php?post_type=featured_content">Featured Content</a> to add or modify content. <br>Add News and Events by using Posts. <br>Pages includes <a href="/wp-admin/post.php?post=29&action=edit">Contact</a>, e101 Prize, Students, Mission and Curriculum and other miscellaneous pages.</li>
+	<li>Click the help tab at the upper right in each section of the Wordpress Admin for specific instructions.</li>
+	<li>To edit the introductory text on front page go to <a href="/wp-admin/widgets.php">Appearance->Widgets</a> and edit the text in the widget called "Front Page Introduction". If the widget is closed, click the triangle to open it for editing. When you click "Save" it will automatically update.</li>
+	<li>SoundCloud options are set for the user "risd-liberal-arts". All sound files for "risd-liberal-arts" will be available for the site if they are uploaded to SoundCloud. SoundCloud files can be inserted by clicking the "Add Media" button and then click "SoundCloud is Gold". On most front end pages a randomly chosen SoundCloud track from the site will be displayed in the right hand sidebar. <a href="http://wordpress.org/extend/plugins/soundcloud-is-gold/">Click for more information.</a></li>
+	</ol>'; 
+	$widget_content .= '<hr />';
+	$widget_content  .=  '<h4>Faculty Profiles</h4>';
+	$widget_content .= '<ol>';
+	$widget_content .= '<li>To change your faculty profile, click on "Faculty" in the left hand menu and then click on your name.</li>
+						<li>Click the help tab at the upper right corner of your page in the Wordpress Admin for more specific instructions.</li>';
+	$widget_content .= '</ol>';
+	$widget_content .= '<hr />';
+	echo $widget_content;
+
+
   //define arguments for WP_Query()
   $qargs = array(
     'post_type'=>'page',
