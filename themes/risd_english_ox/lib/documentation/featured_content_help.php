f883cc690b95512052063416ebed2e697be262da
Daniel Wiener <daniel@danielwiener.com>
2013-04-04 16:33:35 -0400

    modified Feature Content help page

---
+++ themes/risd_english_ox/lib/documentation/featured_content_help.php
@@ -0,0 +1,11 @@
+<h2>How to Add New Featured Content</h2>
+
+<p><strong>Featured Content</strong> is the rotating slideshow that appears on the home page. Keep in mind there can only be <strong>SIX</strong> seperate slides.</p>
+
+<p><strong>Title</strong>: This is the title that appears in all capitals, in a black and white banner, overlaid across the Featured Image. Do not make the title too long, or the banner will wrap. It should be around three to five words.</p>
+
+<p><strong>Excerpt</strong>: This is the text that will appear in smaller font beneath the title banner, and will also overlay the image. Again make your excerpt text brief, so the banner does not wrap.</p>
+
+<p><strong>Featured Image</strong>: This is the image that will appear behind the Title and Excerpt. <em>The image must be 750 x 380 pixels.</em> WordPress will not do this for you - you must adjust the image to these dimensions before uploading. (Note: DO NOT insert this image in the Main Text Box, instead use the Featured Image box.)</p>
+
+<p><strong>Location</strong>: In the Location subpanel on the right side of the screen, you MUST check <strong>Featured</strong>. By checking this box,  the image will now appear on the home page slide show. This step is required. (Note that this is the ONLY time you should be selecting Featured, all other Locations will be either Primary or Seconday.)</p>
