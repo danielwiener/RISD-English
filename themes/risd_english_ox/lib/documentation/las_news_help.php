d1b310ed778479dbb7c98ba3727749f32cbb7fac
Daniel Wiener <daniel@danielwiener.com>
2013-04-04 16:33:48 -0400

    modified news and events help page

---
+++ themes/risd_english_ox/lib/documentation/las_news_help.php
@@ -0,0 +1,30 @@
+<h2>LAS News and Events: How to Upload a New Post</h2>
+
+
+<p>To create a new post, select <strong>Posts, Add New.</strong></p>
+
+
+<p>Enter the <strong>Title</strong>.</p>
+<p>Enter the content of the post in the <strong>Main Text Box</strong>.</p>
+<p>Note: The content can be text or formatted text, and can include images and videos. 
+For general help, see my <a href="http://documentation.diyartportfolios.com/" target="_blank">Documentation - Instructions for WordPress</a> website. You can find instructions for both <a href="http://documentation.diyartportfolios.com/posts/formatting-text" target="_blank">Formatting Text</a>, <a href="http://documentation.diyartportfolios.com/posts/create-a-post-with-text-and-image" target="_blank">Inserting Images</a> and <a href="http://documentation.diyartportfolios.com/!/video" target="_blank">Embedding Videos</a>.</p>
+
+<p>Fill in a short explanatory caption in the <strong>Excerpt</strong>.</p> 
+<p>The <strong>Excerpt</strong> is the shortened version of the text that will appear on the <a href="http://risd-english.com/!/news-events" target="_blank">Table of Contents</a> page, underneath the post's featured image. If you do not write an excerpt, Wordpress will automatically select the first 30 words of the main text. (Although an extra step, it always looks better when the post is summarized into an excerpt, rather than ending in an ellipses.)</p>
+
+<p>In <strong>Categories</strong>, select <strong>News and Events</strong>.</p>
+
+<p>Under <strong>Featured Image</strong>, insert the image you would like to accompany the post.</p> <p>Remember the thumbnail version (on the <a href="http://risd-english.com/!/news-events" target="_blank">Table of Contents</a> page) will be a long formatted image, and automatically sized into a rectangle. (The image will be cropped to 470 x 140 pixels.) Choose an image with action in the middle, because WordPress will automatically crop to the center chopping off the edges of the longest dimension. If you do not enter a Featured Image, a random image will rotate from a pre-selected library of literary-themed images.</p>
+
+Optional: Under <strong>Location</strong>, click <strong>Primary</strong> for the image and title to appear as a "teaser" on the front page of the site, beneath the slideshow. Change these often.
+
+
+<p><strong>Optional:</strong> If you would like the post a news story from another website, you can add an <strong>external link</strong> under <strong>Optional Info, External Url</strong>.</p> 
+<p>This means that when the visitor clicks on the thumbnail or the Title, they will be linked to that external page. Be sure to still summarize the external story you are linking to in the <strong>Excerpt</strong> box, so the post isn't merely an image and title.</p>
+
+<p>Note: On many individual pages throughout the site, there is a side bar that lists the ten most recent News Posts.</p>
+
+
+
+ 
+
