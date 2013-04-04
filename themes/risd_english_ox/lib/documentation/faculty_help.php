3e9aaabfcc6c8b61fe31a3464595667871dc30bf
Daniel Wiener <daniel@danielwiener.com>
2013-04-04 16:32:11 -0400

    modified facuty help page

--- themes/risd_english_ox/lib/documentation/faculty_help.php
+++ themes/risd_english_ox/lib/documentation/faculty_help.php
@@ -1,2 +1,12 @@
-<p>This is where I would provide help to the user on how everything in my admin panel works. Formatted HTML works fine in here too.</p>
-<p>Another paragraph <a href="/url" title="sometitle">Linked Text</a></p>
+<h2>Faculty</h2>
+<p>To <em>add a new</em> faculty member, click <strong>Faculty, Add New</strong>.</p>
+<p>You must fill out the following categories.</p>
+<p><strong>Title:</strong> Enter member's last name.</p>
+<p><strong>Main text box:</strong> Enter member's biographical information.</p>
+<p><strong>Faculty Info, first name:</strong> Enter member's first name. Do not forget this line - if left blank, the member will only have a last name!</p>
+<p><strong>Excerpt:</strong> Enter the short introductory bio you would like to appear on the parent page alongside the member's photograph - if you leave this blank, the first 100 words of the main bio (from the text box above) will be automaticaly selected.</p>
+<p><strong>Faculty Category:</strong> Select either Full Time Faculty, or Senior Lecturer. (if you would like to add another category, please contact Daniel.)</p>
+<p><strong>Featured Image:</strong> Insert member's profile photograph. If the image is larger than 158 x 158 pixels, it will be automatically cropped down to that size for the member's individual page. The thumbnail image will be sized and cropped further, down to 75 x 90 pixels.</p>
+
+
+To <em>edit an existing</em> faculty member's page, click on the member's last name and make the necessary adjustments in the above categories.
