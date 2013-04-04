6161b0901ffb7402e2357ce987f15cbb96d3f5ac
Daniel Wiener <daniel@danielwiener.com>
2013-04-04 16:33:21 -0400

    modified exhibition help page

---
+++ themes/risd_english_ox/lib/documentation/exhibition_help.php
@@ -0,0 +1,32 @@
+<h2>Exhibitions: How To Edit the Introductory Text</h2>
+
+<p>(This section appears as "Department Shows" on the front end.)</p>
+<p>To edit the text that appears at the top of the <a href="http://risd-english.com/exhibitions" target="_blank">Table of Contents</a> page, on the back end select <strong><a href="/wp-admin/widgets.php">Appearances, Widgets</a></strong> on the left toolbar.</p>
+<p>On the upper right side of the screen, click the <strong>down menu arrow</strong> to the right of <strong>Text</strong>. Here you can edit the introductory text. You cannot add any special formatting or links, the text can only appear in paragraph form.</p>
+
+
+<h2>How to Add a New Exhibition</h2>
+<p>On the left toolbar, select <strong>Exhibitions, Add New</strong>.</p>
+<p>Enter the Department Show's <strong>Title</strong>.</p>
+<p>Enter the content of the Department Show in the <strong>Main Text Box</strong>. 
+The content can be text or formatted text, and can include images or video. </p>
+<p>For general help, see my <a href="http://documentation.diyartportfolios.com/" target="_blank">Documentation - Instructions for WordPress</a> website. 
+Further instructions can be found under specific headings, such as <a href="http://documentation.diyartportfolios.com/posts/formatting-text" target="_blank">Formatting Text</a>, <a href="http://documentation.diyartportfolios.com/posts/create-a-post-with-text-and-image" target="_blank">Inserting Images</a> alongside text, and <a href="http://documentation.diyartportfolios.com/!/video" target="_blank">Inserting video</a>.</p>
+
+Since Department Shows will likely include many images, it is also possible to create an image gallery in the content section. For help see WordPress instructions: <a href="http://en.support.wordpress.com/images/gallery/" target="_blank">How to Create an Image Gallery.</a>
+
+<p>Fill in a short explanatory caption of the Exhibition in the <strong>Excerpt</strong>. 
+(The <strong>Excerpt</strong> is the shortened version of the text that will appear on the <a href="risd-english.com/!/news-events" target="_blank">Table of Contents</a> page, underneath the post's featured image. If you do not write an excerpt, Wordpress will automatically select the first 30 words of the main content.)</p>
+
+<p>In the <strong>Featured Image</strong> box, you will need to upload an image that will become the thumbnail image for the Department Show.  This is the cropped image that will appear alongside your  Show Title and Excerpt on the <a href="http://risd-english.com/exhibitions" target="_blank">Table of Contents</a> page. WordPress will automatically crop your Featured Image down to 470 x 140 pixels. Choose an image with action in the middle, because it will be cropped to the center - chopping off the edges of the longest dimension.</p> 
+
+<p>Underneath the Featured Image is the <strong>Location</strong> box, select <strong>Secondary</strong>.</p>
+
+<h2>Updating the Department Show Drop Down Menu</h2>
+
+<p>To ensure Show titles are appearing under the black <strong>Department Show Menu</strong> on the front end (the further options that drop down when the cursor is held over the Department Shows tab), you will need to return to the <strong>Appearance</strong> tab on the left toolbar (where you edited the Introductory Text). </p>
+<p>Under Apperance, select <strong>Menus</strong>. Then find the appropriate box (here it would be Exhibitions) and check the box of the Department Show you would like to add to the drop down menu.</p> 
+
+<p>For step-by-step instructions, see <a href="http://documentation.diyartportfolios.com/menus-navigation/adding-and-deleting-menu-items" target="_blank">Adding and Deleting Menu Items</a>.</p>
+
+Note that when updating the Department Shows Menu, even if you delete it as a menu item, the post (with your main content) will still appear on the Table of Contents page.
