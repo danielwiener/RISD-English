955c0218f520f6f5e5a8892d329563efe0a4a922
Daniel Wiener <daniel@danielwiener.com>
2013-04-06 10:02:04 -0400

    modified faculty help

--- themes/risd_english_ox/lib/documentation/faculty_help.php
+++ themes/risd_english_ox/lib/documentation/faculty_help.php
@@ -4,7 +4,7 @@
 <p><strong>Title:</strong> Enter member's last name.</p>
 <p><strong>Main text box:</strong> Enter member's biographical information.</p>
 <p><strong>Faculty Info, first name:</strong> Enter member's first name. Do not forget this line - if left blank, the member will only have a last name!</p>
-<p><strong>Excerpt:</strong> Enter the short introductory bio you would like to appear on the parent page alongside the member's photograph - if you leave this blank, the first 100 words of the main bio (from the text box above) will be automaticaly selected.</p>
+<p><strong>Excerpt:</strong> Enter the short introductory bio you would like to appear on the parent page alongside the member's photograph - if you leave this blank, the first 30 words of the main bio (from the text box above) will be automaticaly selected.</p>
 <p><strong>Faculty Category:</strong> Select either Full Time Faculty, or Senior Lecturer. (if you would like to add another category, please contact Daniel.)</p>
 <p><strong>Featured Image:</strong> Insert member's profile photograph. If the image is larger than 158 x 158 pixels, it will be automatically cropped down to that size for the member's individual page. The thumbnail image will be sized and cropped further, down to 75 x 90 pixels.</p>
 
