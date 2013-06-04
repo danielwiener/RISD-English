9bbdfd5884922cb2a0b71bb5103803666819c171
Daniel Wiener <daniel@danielwiener.com>
2013-06-04 12:12:16 -0400

    adding google analytics javascript

--- themes/risd_english_ox/footer.php
+++ themes/risd_english_ox/footer.php
@@ -65,6 +65,19 @@
 	<?php do_atomic( 'close_body' ); // oxygen_close_body ?>
 	
 	<?php wp_footer(); // wp_footer ?>
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-40325017-1']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
 
 </body>
 </html>
